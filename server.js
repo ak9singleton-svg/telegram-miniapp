027bba8cf7731021c3eeb5d01ae7329163178fe0
ak9singleton-svg <ak9singleton@gmail.com>
2025-11-12 23:03:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const compression = require('compression');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(compression()); // Gzip сжатие всех ответов
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
